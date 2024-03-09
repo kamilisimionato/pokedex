84807b5c5912045b74427129ea1f1820d23e2c19
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2024-03-09 11:16:17 -0300

    Dúvida na página 146

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -29,12 +29,30 @@ public class HomeController : Controller
             tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
         }
         ViewData["Tipos"] = tipos;
-        return View(pokemons);
+        return View(pokemons);   
     }
 
     public IActionResult Details(int id)
     {
-        return View();
+        List<Pokemon> pokemons = [];
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo> tipos = [];
+        using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        DetailsVM details = new() {
+            Tipos = tipos,
+            Atual = pokemons.FirstOrDefault(p => p.Numero == id),
+            Anterior = pokemons.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
+            Proximo = pokemons.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),          
+        };
+        return View(details);
     }
     
     public IActionResult Privacy()
